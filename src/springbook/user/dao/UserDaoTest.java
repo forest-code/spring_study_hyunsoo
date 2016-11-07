18cbe9b6601a9db4b1b4a4cf50b087fe230ca436
hyunto <eqelizer@naver.com>
2016-11-07 19:12:58 +0900

    2.3.5 finish

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -16,6 +16,9 @@ import springbook.user.domain.User;
 public class UserDaoTest {
 	
 	private UserDao dao;
+	private User user1;
+	private User user2;
+	private User user3;
 	
 	@Before
 	public void setUp() {
@@ -24,13 +27,14 @@ public class UserDaoTest {
 //		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");		
 		
 		this.dao = context.getBean("userDao", UserDao.class);
+		
+		this.user1 = new User("gyumee", "박성철", "springno1");
+		this.user2 = new User("leegw700", "이길원", "springno2");
+		this.user3 = new User("bumjin", "박범진", "springno3");
 	}
 	
 	@Test
 	public void addAndGet() throws SQLException {
-		
-		User user1 = new User("gyumee", "박성철", "springno1");
-		User user2 = new User("leegw700", "이길원", "springno2");
 		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
@@ -50,10 +54,6 @@ public class UserDaoTest {
 	
 	@Test
 	public void count() throws SQLException {
-		
-		User user1 = new User("gyumee", "박성철", "springno1");
-		User user2 = new User("leegw700", "이길원", "springno2");
-		User user3 = new User("bumjin", "박범진", "springno3");
 		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
