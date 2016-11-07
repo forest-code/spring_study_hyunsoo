9276b368e61af155169dbf1e271476ec156c3b6f
hyunto <eqelizer@naver.com>
2016-11-07 21:01:58 +0900

    2.4.2 finish
    - 운영용, 개발용 어플리케이션 컨텍스트 분리

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -9,8 +9,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -18,7 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="/applicationContext.xml")
+@ContextConfiguration(locations="/test-applicationContext.xml")
 public class UserDaoTest {
 	
 	@Autowired
