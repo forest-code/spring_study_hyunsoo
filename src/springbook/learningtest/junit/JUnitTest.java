934c6d9de7b4cfdb448b25d194df3b19b1f85250
hyunto <eqelizer@naver.com>
2016-11-07 22:20:37 +0900

    finish chapter 2
    - 학습 테스트 코드 추가

---
+++ src/springbook/learningtest/junit/JUnitTest.java
@@ -0,0 +1,119 @@
+package springbook.learningtest.junit;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.matchers.JUnitMatchers.either;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/junit.xml")
+public class JUnitTest {
+
+	@Autowired
+	ApplicationContext context;
+
+	/* First Test */
+	static JUnitTest testObject;
+
+	/* Second Test */
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+
+	/* Third Test */
+	static ApplicationContext contextObject = null;
+
+	@Test	
+	public void firstTest1() {
+		System.out.println("before first-test1 : " + testObject);
+		assertThat(this, is(not(sameInstance(testObject))));
+		testObject = this;
+		System.out.println("after first-test1 : " + testObject);
+		System.out.println();
+	}
+
+	@Test
+	public void firstTest2() {
+		System.out.println("before first-test2 : " + testObject);
+		assertThat(this, is(not(sameInstance(testObject))));
+		testObject = this;
+		System.out.println("after first-test2 : " + testObject);
+		System.out.println();
+	}
+
+	@Test
+	public void firstTest3() {
+		/* First Test */
+		System.out.println("before first-test3 : " + testObject);
+		assertThat(this, is(not(sameInstance(testObject))));
+		testObject = this;
+		System.out.println("after first-test3 : " + testObject);
+		System.out.println();
+	}
+	
+	@Test
+	public void secondTest1() {
+		System.out.println("before second-test1 : " + testObjects);
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		System.out.println("after second-test1 : " + testObjects);
+		System.out.println();
+	}
+	
+	@Test
+	public void secondTest2() {
+		System.out.println("before second-test2 : " + testObjects);
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		System.out.println("after second-test2 : " + testObjects);
+		System.out.println();
+	}
+	
+	@Test
+	public void secondTest3() {
+		System.out.println("before second-test3 : " + testObjects);
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		System.out.println("after second-test3 : " + testObjects);
+		System.out.println();
+	}
+	
+	@Test
+	public void thirdTest1() {
+		System.out.println("before third-test1 : " + contextObject);
+		assertThat(contextObject == null || contextObject == this.context, is(true));
+		contextObject = this.context;
+		System.out.println("after third-test1 : " + contextObject);
+		System.out.println();
+	}
+	
+	@Test
+	public void thirdTest2() {
+		System.out.println("before third-test2 : " + contextObject);
+		assertTrue(contextObject == null || contextObject == this.context);
+		contextObject = this.context;
+		System.out.println("after third-test2 : " + contextObject);
+		System.out.println();
+	}
+	
+	@Test
+	public void thirdTest3() {
+		System.out.println("before third-test3 : " + contextObject);
+		assertThat(contextObject, either(is(nullValue())).or(is(this.context)));
+		contextObject = this.context;
+		System.out.println("after third-test3 : " + contextObject);
+		System.out.println();
+	}
+}
