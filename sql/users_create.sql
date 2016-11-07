73418bb124fdd102a9487f558d79746c9c3a8172
hyunto <eqelizer@naver.com>
2016-11-07 20:53:43 +0900

    운영용, 테스트용 DB 생성문 작성

--- sql/users_create.sql
+++ sql/users_create.sql
@@ -1,5 +1,25 @@
+-- for operation
+create database if not exists springbook;
+
+use springbook;
+
+drop table if exists users;
+
 create table users (
 	id varchar(10) primary key,	
 	name varchar(20) not null,
 	password varchar(10) not null
-)
+);
+
+create database if not exists testdb;
+
+-- for test
+use testdb;
+
+drop table if exists users;
+
+create table users (
+	id varchar(10) primary key,	
+	name varchar(20) not null,
+	password varchar(10) not null
+);
