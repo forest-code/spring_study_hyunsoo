de41cac4c907c819dfe878cf242b01b8354e20a9
hyunto <eqelizer@naver.com>
2016-11-22 00:59:41 +0900

    3.1 finish
    - JDBC 코드에 예외처리 추

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -38,15 +38,15 @@ public class UserDao {
 		ps.setString(1, id);
 
 		ResultSet rs = ps.executeQuery();
-		
-		/*Before*/
-//		rs.next();
-//		User user = new User();
-//		user.setId(rs.getString("id"));
-//		user.setName(rs.getString("name"));
-//		user.setPassword(rs.getString("password"));
 
-		/*After*/
+		/* Before */
+		// rs.next();
+		// User user = new User();
+		// user.setId(rs.getString("id"));
+		// user.setName(rs.getString("name"));
+		// user.setPassword(rs.getString("password"));
+
+		/* After */
 		User user = null;
 		if (rs.next()) {
 			user = new User();
@@ -54,43 +54,79 @@ public class UserDao {
 			user.setName(rs.getString("name"));
 			user.setPassword(rs.getString("password"));
 		}
-		
+
 		rs.close();
 		ps.close();
 		c.close();
-		
+
 		if (user == null)
 			throw new EmptyResultDataAccessException(1);
 
 		return user;
 	}
-	
+
 	public void deleteAll() throws SQLException {
-		Connection c = this.dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("delete from users");
-		ps.executeUpdate();
-		
-		ps.close();
-		c.close();
-	}
-	
-	public int getCount() throws SQLException {
-		Connection c = this.dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("select count(*) from users");
-		ResultSet rs = ps.executeQuery();
-		
-		int count = 0;
-		while(rs.next()) {
-			count = rs.getInt(1);
+		Connection c = null;
+		PreparedStatement ps = null;
+
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("delete from users");
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
 		}
-		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		return count; 
+	}
+
+	public int getCount() throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("select count(*) from users");
+			rs = ps.executeQuery();
+			rs.next();
+			return rs.getInt(1);
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+
+			}
+
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+
+				}
+			}
+
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+
+				}
+			}
+		}
 	}
 
 }
