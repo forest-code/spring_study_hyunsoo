89d60a7af0ce86962d967ac9c7db1cc8d0bdf3a2
hyunto <eqelizer@naver.com>
2016-11-22 01:22:21 +0900

    3.2.2 extract method 리팩토링 적용

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -71,7 +71,10 @@ public class UserDao {
 
 		try {
 			c = dataSource.getConnection();
-			ps = c.prepareStatement("delete from users");
+			
+//			ps = c.prepareStatement("delete from users");
+			ps = makeStatement(c);
+			
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			throw e;
@@ -89,6 +92,12 @@ public class UserDao {
 				}
 			}
 		}
+	}
+	
+	private PreparedStatement makeStatement(Connection c) throws SQLException {
+		PreparedStatement ps;
+		ps = c.prepareStatement("delete from users");
+		return ps;
 	}
 
 	public int getCount() throws SQLException {
