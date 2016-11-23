d184bfa97f64cee4b0446b4b3d2a4c2b684225aa
hyunto <eqelizer@naver.com>
2016-11-24 01:38:55 +0900

    3.4.1 진행중

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -17,9 +17,15 @@ public class UserDao {
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
+	
+	private JdbcContext jdbcContext;
+	
+	public void setJdbcContext(JdbcContext jdbcContext) {
+		this.jdbcContext = jdbcContext;
+	}
 
 	public void add(final User user) throws SQLException {
-		jdbcContextWithStatementStrategy(new StatementStrategy() {
+		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
 
 			@Override
 			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
@@ -59,7 +65,7 @@ public class UserDao {
 	}
 
 	public void deleteAll() throws SQLException {
-		jdbcContextWithStatementStrategy(new StatementStrategy() {
+		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
 
 			@Override
 			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
@@ -101,34 +107,6 @@ public class UserDao {
 					c.close();
 				} catch (SQLException e) {
 
-				}
-			}
-		}
-	}
-
-	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-
-		try {
-			c = dataSource.getConnection();
-
-			ps = stmt.makePreparedStatement(c);
-
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
 				}
 			}
 		}
