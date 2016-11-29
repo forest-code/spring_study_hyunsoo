e4ca86b471774ce76b2f1af8372cc42a77a0157f
hyunto <eqelizer@naver.com>
2016-11-29 22:23:55 +0900

    3.6.4 finish

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -1,99 +1,85 @@
 package springbook.user.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
 public class UserDao {
-	
+
 	private DataSource dataSource;
-	
-	private JdbcContext jdbcContext;
+
+	private JdbcTemplate jdbcTemplate;
 
 	public void setDataSource(DataSource dataSource) {
-		this.jdbcContext = new JdbcContext();
-		
-		this.jdbcContext.setDataSource(dataSource);
-		
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+
 		this.dataSource = dataSource;
 	}
-	
+
 	public void add(final User user) throws SQLException {
-		this.jdbcContext.executeSql("insert into users(id, name, password) values (?,?,?)", user.getId(), user.getName(), user.getPassword());
+		this.jdbcTemplate.update("insert into users(id, name, password) values (?,?,?)", user.getId(), user.getName(),
+				user.getPassword());
 	}
-	
+
 	public void deleteAll() throws SQLException {
-		this.jdbcContext.executeSql("delete from users");
+
+		// this.jdbcTemplate.update(new PreparedStatementCreator() {
+		// public PreparedStatement createPreparedStatement(Connection con)
+		// throws SQLException {
+		// return con.prepareStatement("delete from users");
+		// }
+		// });
+
+		this.jdbcTemplate.update("delete from users");
 	}
-	
+
 	public User get(String id) throws SQLException {
-		Connection c = this.dataSource.getConnection();
-		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-		ps.setString(1, id);
 
-		ResultSet rs = ps.executeQuery();
-
-		User user = null;
-		if (rs.next()) {
-			user = new User();
-			user.setId(rs.getString("id"));
-			user.setName(rs.getString("name"));
-			user.setPassword(rs.getString("password"));
-		}
-
-		rs.close();
-		ps.close();
-		c.close();
-
-		if (user == null)
-			throw new EmptyResultDataAccessException(1);
-
-		return user;
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] { id },
+				new RowMapper<User>() {
+					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+						User user = new User();
+						user.setId(rs.getString("id"));
+						user.setName(rs.getString("name"));
+						user.setPassword(rs.getString("password"));
+						return user;
+					}
+				});
 	}
 
 	public int getCount() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
 
-		try {
-			c = dataSource.getConnection();
-			ps = c.prepareStatement("select count(*) from users");
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			try {
-				rs.close();
-			} catch (SQLException e) {
-
-			}
-
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-
-				}
-			}
-
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-
-				}
-			}
-		}
+//		return this.jdbcTemplate.query(new PreparedStatementCreator() {
+//			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//				return con.prepareStatement("select count(*) from users");
+//			}
+//		}, new ResultSetExtractor<Integer>() {
+//			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+//				rs.next();
+//				return rs.getInt(1);
+//			}
+//		});
+		
+		return this.jdbcTemplate.queryForInt("select count(*) from users");
 	}
 
+	public List<User> getAll() {
+		return this.jdbcTemplate.query("select * from users order by id", 
+				new RowMapper<User>() {
+					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+						User user = new User();
+						user.setId(rs.getString("id"));
+						user.setName(rs.getString("name"));
+						user.setPassword(rs.getString("password"));
+						return user;
+					}
+		});
+	}
 }
