1e0e9f1da758964fc5906250e0c2cc91792a325c
hyunto <eqelizer@naver.com>
2016-11-30 09:11:19 +0900

    finish chapter 3

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -13,73 +13,39 @@ import springbook.user.domain.User;
 
 public class UserDao {
 
-	private DataSource dataSource;
-
 	private JdbcTemplate jdbcTemplate;
 
-	public void setDataSource(DataSource dataSource) {
+	public void setJdbcTemplate(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
-
-		this.dataSource = dataSource;
 	}
 
 	public void add(final User user) throws SQLException {
-		this.jdbcTemplate.update("insert into users(id, name, password) values (?,?,?)", user.getId(), user.getName(),
-				user.getPassword());
+		this.jdbcTemplate.update("insert into users(id, name, password) values (?,?,?)", user.getId(), user.getName(), user.getPassword());
 	}
 
 	public void deleteAll() throws SQLException {
-
-		// this.jdbcTemplate.update(new PreparedStatementCreator() {
-		// public PreparedStatement createPreparedStatement(Connection con)
-		// throws SQLException {
-		// return con.prepareStatement("delete from users");
-		// }
-		// });
-
 		this.jdbcTemplate.update("delete from users");
 	}
 
 	public User get(String id) throws SQLException {
-
-		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] { id },
-				new RowMapper<User>() {
-					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-						User user = new User();
-						user.setId(rs.getString("id"));
-						user.setName(rs.getString("name"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-				});
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] { id }, this.userMapper);
 	}
 
 	public int getCount() throws SQLException {
-
-//		return this.jdbcTemplate.query(new PreparedStatementCreator() {
-//			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//				return con.prepareStatement("select count(*) from users");
-//			}
-//		}, new ResultSetExtractor<Integer>() {
-//			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-//				rs.next();
-//				return rs.getInt(1);
-//			}
-//		});
-		
 		return this.jdbcTemplate.queryForInt("select count(*) from users");
 	}
 
 	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id", 
-				new RowMapper<User>() {
-					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-						User user = new User();
-						user.setId(rs.getString("id"));
-						user.setName(rs.getString("name"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-		});
+		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
 	}
+	
+	private RowMapper<User> userMapper = new RowMapper<User>() {
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+			return user;
+		}
+	};
 }
