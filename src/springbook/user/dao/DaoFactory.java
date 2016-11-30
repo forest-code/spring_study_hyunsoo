05955c820af0d56713c206d6bc351b3bfb0d18f1
hyunto <eqelizer@naver.com>
2016-11-30 11:35:07 +0900

    daofactory 수정

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -23,7 +23,7 @@ public class DaoFactory {
 	@Bean
 	public UserDao userDao() {
 		UserDao userDao = new UserDao();
-		userDao.setDataSource(dataSource());
+		userDao.setJdbcTemplate(dataSource());
 		return userDao;
 	}
 }
