8b0c128129ed2f739dacc323e18b4eed97ee0074
hyunto <eqelizer@naver.com>
2017-01-13 01:19:08 +0900

    5.1.4 finish
    - User Level이 지정되지 않은 경우 BASIC으로 초기화하는 로직 추가

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -38,4 +38,11 @@ public class UserService {
 			}
 		}
 	}
+	
+	public void add(User user) {
+		if (user.getLevel() == null) {
+			user.setLevel(Level.BASIC);
+		}
+		userDao.add(user);
+	}
 }
--- src/test/java/springbook/user/service/UserServiceTest.java
+++ src/test/java/springbook/user/service/UserServiceTest.java
@@ -61,4 +61,22 @@ public class UserServiceTest {
 		User userUpdate = userDao.get(user.getId());
 		assertThat(userUpdate.getLevel(), is(expectedLevel));
 	}
+	
+	@Test
+	public void add() {
+		userDao.deleteAll();
+		
+		User userWithLevel = users.get(4);
+		User userWithoutLevel = users.get(0);
+		userWithoutLevel.setLevel(null);
+		
+		userService.add(userWithLevel);
+		userService.add(userWithoutLevel);
+		
+		User userWithLevelRead = userDao.get(userWithLevel.getId());
+		User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+		
+		assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel()));
+		assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
+	}
 }
