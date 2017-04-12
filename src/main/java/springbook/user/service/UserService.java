bcfdecfe5f14c2fa6583be573083b2b28c2bed1a
hyunto <eqelizer@naver.com>
2017-04-12 23:02:45 +0900

    6.7 완료
    - @Transactional Annotation 적용

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -2,14 +2,20 @@ package springbook.user.service;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import springbook.user.domain.User;
 
+@Transactional
 public interface UserService {
 
 	void add(User user);
 	
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=true, timeout=30)
 	User get(String id);
 	
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=true, timeout=30)
 	List<User> getAll();
 	
 	void deleteAll();
