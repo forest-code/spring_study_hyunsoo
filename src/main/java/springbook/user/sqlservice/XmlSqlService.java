3969a3ed230679a4935c0214265ee069e2919752
hyunto <eqelizer@naver.com>
2017-05-05 01:03:09 +0900

    7.2.3 완료
    - SQL문을 Load하는 빈 초기화 로직 리팩토링 : 생성자 복잡성 제거, XML파일 이름 주입

--- src/main/java/springbook/user/sqlservice/XmlSqlService.java
+++ src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -16,13 +17,20 @@ public class XmlSqlService implements SqlService {
 	
 	private Map<String, String> sqlMap = new HashMap<String, String>();
 	
-	public XmlSqlService() {
+	private String sqlmapFile;
+	
+	public void setSqlmapFile(String sqlmapFile) {
+		this.sqlmapFile = sqlmapFile;
+	}
+	
+	@PostConstruct
+	public void loadSql() {
 		String contextPath = Sqlmap.class.getPackage().getName();
 		
 		try {
 			JAXBContext context = JAXBContext.newInstance(contextPath);
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+			InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
 			Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 			
 			for (SqlType sql : sqlmap.getSql()) {
