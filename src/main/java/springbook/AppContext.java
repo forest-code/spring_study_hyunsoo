aee292d115008adbf8851160d7036ee1369edca5
hyunto <eqelizer@naver.com>
2017-05-18 02:10:19 +0900

    7.6.5 완료
    - @Value 어노테이션을 사용한 Property 값 설정

--- src/main/java/springbook/AppContext.java
+++ src/main/java/springbook/AppContext.java
@@ -2,14 +2,14 @@ package springbook;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -33,21 +33,31 @@ public class AppContext {
 	 * DB 연결과 트랜잭션
 	 */
 	
-	@Autowired
-	Environment env;
+	@Value("${db.driverClass}")
+	private Class<? extends Driver> driverClass;
+	
+	@Value("${db.url}")
+	private String url;
+	
+	@Value("${db.username}")
+	private String username;
+	
+	@Value("${db.password}")
+	private String password;
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 	
 	@Bean
 	public DataSource dataSource() {
 		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 		
-		try {
-			dataSource.setDriverClass((Class<? extends java.sql.Driver>) Class.forName(env.getProperty("db.driverClass")));
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-		dataSource.setUrl(env.getProperty("db.url"));
-		dataSource.setUsername(env.getProperty("db.username"));
-		dataSource.setPassword(env.getProperty("db.password"));
+		dataSource.setDriverClass(this.driverClass);
+		dataSource.setUrl(this.url);
+		dataSource.setUsername(this.username);
+		dataSource.setPassword(this.password);
 		
 		return dataSource;
 	}
