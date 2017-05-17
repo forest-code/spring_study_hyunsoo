a2882e0af067c51622dfd91fb3bb3c61646fd2ae
hyunto <eqelizer@naver.com>
2017-05-18 02:02:36 +0900

    7.6.5 진행 중
    - @PropertySource와 properties 파일을 이용한 DB Connection 정보 설정

--- src/main/java/springbook/AppContext.java
+++ src/main/java/springbook/AppContext.java
@@ -2,11 +2,14 @@ package springbook;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -23,20 +26,28 @@ import springbook.user.service.UserServiceTest.TestUserService;
 @EnableTransactionManagement
 @ComponentScan(basePackages = "springbook.user")
 @Import(SqlServiceContext.class)
+@PropertySource("/database.properties")
 public class AppContext {
 	
 	/**
 	 * DB 연결과 트랜잭션
 	 */
 	
+	@Autowired
+	Environment env;
+	
 	@Bean
 	public DataSource dataSource() {
 		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 		
-		dataSource.setDriverClass(Driver.class);
-		dataSource.setUrl("jdbc:mysql://localhost/testdb?characterEncoding=UTF-8");
-		dataSource.setUsername("spring");
-		dataSource.setPassword("book");
+		try {
+			dataSource.setDriverClass((Class<? extends java.sql.Driver>) Class.forName(env.getProperty("db.driverClass")));
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		dataSource.setUrl(env.getProperty("db.url"));
+		dataSource.setUsername(env.getProperty("db.username"));
+		dataSource.setPassword(env.getProperty("db.password"));
 		
 		return dataSource;
 	}
